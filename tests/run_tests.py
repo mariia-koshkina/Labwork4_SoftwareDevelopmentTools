139aca20730517fda3541d23ee26f851824642d2
mariia-koshkina <koshkina.masha2007@gmail.com>
2025-12-18 20:42:48 +0300

    Added run_tests.py

---
+++ tests/run_tests.py
@@ -0,0 +1,9 @@
+import unittest
+
+from tests.test_circle import CircleTestCase
+from tests.test_square import SquareTestCase
+from tests.test_rectangle import RectangleTestCase
+from tests.test_triangle import TriangleTestCase
+
+if __name__ == "__main__":
+    unittest.main()
